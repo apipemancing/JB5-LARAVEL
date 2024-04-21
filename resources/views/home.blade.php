2686bcc4786e18a6f99c350636002fedc7637e30
apipemancing <tugasapop@gmail.com>
2024-04-21 18:46:59 +0700

    revisi crud 10

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Awal</title>
+</head>
+<body>
+    <h1>Selamat datang di web Random Meme</h1>
+    <h3>oleh : ApiipMc</h3>
+    <h3>Link Meme dibawah</h3> <a href="{{ url('/posts') }}">bwehehehe</a>
+</body>
+</html>
